f75c9ba3ee72f00c8087512eaf28aace04d24379
cyihuoma <ladyihuoma@gmail.com>
2024-10-08 14:42:00 +0100

    E6promises

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+
+export default function getFullResponseFromAPI(promise) {
+    return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
